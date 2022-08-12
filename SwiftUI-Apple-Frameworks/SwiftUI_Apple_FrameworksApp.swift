865f89e8bcc3a6996b4d7b1768bc8e2d5bb302a7
CanerCagri <canerrcagrii@gmail.com>
2022-08-12 19:47:53 +0300

    Initial Commit

---
+++ SwiftUI-Apple-Frameworks/SwiftUI_Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apple_FrameworksApp.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Caner Çağrı on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
